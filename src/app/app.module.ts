cb336c367e16ed05c4514e6bc19c6c224fd92c68
Amila Ariyarathna <amila03ac@gmail.com>
2021-07-10 22:03:08 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MemeListComponent } from './meme-list/meme-list.component';
+import { MemeCardComponent } from './meme-card/meme-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemeListComponent,
+    MemeCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/meme.ts
@@ -0,0 +1,8 @@
+export interface Meme {
+  id: number;
+  name: string;
+  url: string;
+  width: number;
+  height: number;
+  box_count: number;
+}
